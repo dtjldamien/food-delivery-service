ee64efc8dbb6f4191c095b7d86646d107d01a432
Aaron Goh <aarongjy97@gmail.com>
2020-03-29 02:57:14 +0800

    added login and logout for customer

--- customer/src/Categories.js
+++ customer/src/Categories.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import axios from 'axios';
-import {Card} from 'primereact/card';
+import { Card } from 'primereact/card';
 import { Link } from 'react-router-dom';
 
 class Categories extends React.Component {
