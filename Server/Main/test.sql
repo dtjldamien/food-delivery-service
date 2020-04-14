46e45edc2d4e876a04bafe68b727cdff064dbe38
Aaron Goh <aarongjy97@gmail.com>
2020-04-14 20:57:32 +0800

    Fixed Test.sql, moved all to Server/Main

--- Server/Main/test.sql
+++ Server/Main/test.sql
@@ -18,20 +18,20 @@ COMMIT;
 
 BEGIN;
 
-INSERT INTO RestaurantStaffs VALUES (9, 'rsa@email.com', 'password', 'A');
+INSERT INTO RestaurantStaffs VALUES (1, 'rsa@email.com', 'password', 'A');
 INSERT INTO DeliveryRiders VALUES ('dra@email.com', 'A', 'SHD2020C', 00000, 'password');
 INSERT INTO FDSManagers VALUES ('fdsma', 'password');
 
 INSERT INTO FoodItems (fname, description, price, catid) VALUES
-('Fish & Chips', 'Straight from England!', 2.5, 9),
-('Chirashi Don', 'Japanese Food', 5.5, 10),
-('Kimchi Soup', 'Annyeong', 3.5, 11),
-('McSpicy', '$6 meal!', 6.0, 12);
+('Fish & Chips', 'Straight from England!', 2.5, 'Western'),
+('Chirashi Don', 'Japanese Food', 5.5, 'Japanese'),
+('Kimchi Soup', 'Annyeong', 3.5, 'Korean'),
+('McSpicy', '$6 meal!', 6.0, 'Fast Food');
 
 INSERT INTO Sells (rid, fid, availability, dailyLimit) VALUES
-(9, 1, 10, 10),
-(10, 2, 20, 20),
-(11, 3, 30, 30),
-(12, 4, 40, 40);
+(1, 1, 10, 10),
+(2, 2, 20, 20),
+(3, 3, 30, 30),
+(4, 4, 40, 40);
 
 COMMIT;
