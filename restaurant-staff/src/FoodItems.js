1e1af5293774e0394b540de3e6749ba1bc1e53ab
Wong Jin Fu Shaun <sw_best@hotmail.com>
2020-04-16 07:01:43 +0800

    Added update food item
    Commented out the problematic parts first as I keep getting a binding typescript error as of now

--- restaurant-staff/src/FoodItems.js
+++ restaurant-staff/src/FoodItems.js
@@ -12,6 +12,10 @@ class FoodItems extends React.Component {
         this.state = {
             foodData: [],
         }
+        // this.handleChange = this.handleChange.bind(this)
+        // this.handleUpdate = this.handleUpdate.bind(this)
+        // this.updateFoodItem = this.updateFoodItem.bind(this)
+        // this.updateFoodItemDialog = this.viewFoodItemDialog.bind(this)
     }
 
     componentDidMount() {
@@ -23,9 +27,77 @@ class FoodItems extends React.Component {
         axios.get('/api/get/getFoodItemsByRestaurantID', {params: data})
             .then(rsp => this.setState({foodData: rsp.data}))
             .catch(err => console.log(err))
+
+    }
+
+    handleUpdate = async (event, fid) => {
+        event.preventDefault();
+    
+        const data = {
+            fid: fid,
+            fname: this.state.foodData[fid][0],
+            description: this.state.foodData[fid][1],
+            price: this.state.foodData[fid][2],
+        }
+    
+        /* Checks if data has been changed */
+        if (data.fname === this.state.foodData[fid][1] &&
+            data.description === this.state.foodData[fid][2] &&
+            data.price === this.state.foodData[fid][3]) {
+                alert("Food Item Details Not Changed.")
+            } else {
+                await axios.put('/api/put/updateFoodItem', {params: data})
+                    .catch(err => {
+                        console.log(err)
+                        alert("Error Ocurred During Update")
+                    })
+            }
+        }
+
+    handleChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+        console.log(this.state)
+    }
+
+    updateFoodItem(fid) {
+        return (
+            <div>
+                <h4>Update Food Item</h4>
+
+                <form onSubmit={this.handleUpdate}>
+                    <label>fid:         {this.state.foodData[fid][0]}</label>
+                    <br></br>
+                    <label>Food Name:     <input type="text" value={this.state.foodData[fid][1]} name="fname" onChange={this.handleChange}></input></label>
+                    <br></br>
+                    <label>Description:     <input type="text" value={this.state.foodData[fid][2]} name="description" onChange={this.handleChange}></input></label>
+                    <br></br>
+                    <label>Price:     <input type="text" value={this.state.foodData[fid][3]} name="price" onChange={this.handleChange}></input></label>
+                    <br></br>
+                    <input type="submit" value="Update Food Item"/>
+                </form>
+            </div>
+        )
+    }
+
+    updateFoodItemDialog(rowData, column) {
+
+        return (
+            <div>
+                <Dialog header="Update Food Item" visible={this.state.visible} onHide={() => this.setState({visible: false})}>
+                    {this.updateFoodItem()}
+                </Dialog>
+                <Button label=" Edit Food Item" onClick={()=> {
+                        this.setState({visible: true})
+                        this.updateFoodItem(rowData.fid)
+                    }}></Button>
+            </div>
+        )
     }
 
     render() {
+        console.log(this.state.foodData)
         return (
             <div>
                 <DataTable value = {this.state.foodData}>
@@ -33,7 +105,7 @@ class FoodItems extends React.Component {
                     <Column field="fname" header="fname" sortable={true}/>
                     <Column field="description" header= "description" sortable={true}/>
                     <Column field="price" header="price" sortable={true}/>
-                    <Column field="quantity" header="quantity" sortable={true}/>
+                    {/* <Column field="fid" body={this.updateFoodItemDialog}/> */}
                 </DataTable>
             </div>
         )
